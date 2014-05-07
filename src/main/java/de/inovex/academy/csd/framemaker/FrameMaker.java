5c7c6871e018e19af9ce701ac72a2691289844a3
Torsten Flatter <torsten.flatter@inovex.de>
2014-05-07 14:31:51 +0200

    refactored

--- src/main/java/de/inovex/academy/csd/framemaker/FrameMaker.java
+++ src/main/java/de/inovex/academy/csd/framemaker/FrameMaker.java
@@ -6,36 +6,30 @@ import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 
 public class FrameMaker {
-	public List<String> makeFrame(List<String> lines) {
-		ArrayList<String> frameLines = new ArrayList<>();
-		int maxLen = maxLen(lines) ;
-		String block = blockOf(maxLen + 2);
-		frameLines.add(block);
-		for (String line : lines) {
-			frameLines.add("*" + rpad(line, maxLen) + "*");
-		}
-		frameLines.add(block);
-		return frameLines;
-	}
 
-	private String blockOf(int len) {
-		return StringUtils.repeat("*", len);
-	}
+    
+    public List<String> makeFrame(List<String> lines) {
+        ArrayList<String> frameLines = new ArrayList<>();
+        int maxLen = maxLen(lines);
+        String block = blockLine("", maxLen, '*');
+        
+        frameLines.add(block);
+        for (String line : lines) {
+            frameLines.add(blockLine(line, maxLen, ' '));
+        }
+        frameLines.add(block);
+        return frameLines;
+    }
 
-	private String rpad(String in, int len) {
-		StringBuilder ret = new StringBuilder();
-		ret.append(in);
-		for (int i=in.length(); i < len; i++) {
-			ret.append(" ");
-		}
-		return ret.toString();
-	}
-	
-	private int maxLen(Iterable<String> strings) {
-		int maxLen = 0;
-		for (String s : strings) {
-			maxLen = Math.max(maxLen, s.length());
-		}
-		return maxLen;
-	}
+    private String blockLine(String in, int len, char padChar) {
+        return "*" + StringUtils.rightPad(in, len, padChar) + "*";
+    }
+
+    private int maxLen(Iterable<String> strings) {
+        int maxLen = 0;
+        for (String s : strings) {
+            maxLen = Math.max(maxLen, s.length());
+        }
+        return maxLen;
+    }
 }
