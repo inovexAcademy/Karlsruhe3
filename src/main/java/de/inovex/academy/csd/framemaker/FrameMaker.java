0b9d92b69620f228dbc9c3dbc66c0a0eb03a9d24
Torsten Flatter <torsten.flatter@inovex.de>
2014-05-07 14:43:22 +0200

    refactored

--- src/main/java/de/inovex/academy/csd/framemaker/FrameMaker.java
+++ src/main/java/de/inovex/academy/csd/framemaker/FrameMaker.java
@@ -3,33 +3,20 @@ package de.inovex.academy.csd.framemaker;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
-
 public class FrameMaker {
 
     public List<String> makeFrame(List<String> lines) {
         ArrayList<String> frameLines = new ArrayList<>();
         int maxLen = maxLen(lines);
+        Blocker blocker = new Blocker(maxLen, this);
 
-        frameLines.add(makeBlock(maxLen));
+        frameLines.add(blocker.makeBlock());
         for (String line : lines) {
-            frameLines.add(makeLine(line, maxLen));
+            frameLines.add(blocker.makeLine(line));
         }
-        frameLines.add(makeBlock(maxLen));
-        
+        frameLines.add(blocker.makeBlock());
+
         return frameLines;
-    }
-
-    private String makeLine(String line, int maxLen) {
-        return blockLine(line, maxLen, ' ');
-    }
-
-    private String makeBlock(int maxLen) {
-        return blockLine("", maxLen, '*');
-    }
-
-    private String blockLine(String in, int len, char padChar) {
-        return "*" + StringUtils.rightPad(in, len, padChar) + "*";
     }
 
     private int maxLen(Iterable<String> strings) {
